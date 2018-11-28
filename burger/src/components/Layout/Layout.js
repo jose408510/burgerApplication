3401d3537f8ea6e79e157bd08a8ed07bed28a472
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2018-11-27 16:15:02 -0800

    fixed weird issue

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Aux from '../../hoc/Aux'
-import classes from './Layout.css'
 
-const Layout = (props) => (
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+
+const layout = ( props ) => (
     <Aux>
-        <div>Toolbar , SideDrawer , BackDrop</div>
+        <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
-)
+);
 
-export default Layout;
+export default layout;
