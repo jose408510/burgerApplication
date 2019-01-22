7d3eef48e1bbd9e880fe51007d1fd7cd0558bd92
Jose Castellanos <josecastellanos@Joses-MBP-2.attlocal.net>
2019-01-22 10:59:40 -0800

    added more changes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer"
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component { 
-        state = {
-            showSideDrawer: true
-        }
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false})
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
     }
-    render() {
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
         return (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer open={this.state.showSideDrawer}
-        closed={this.sideDrawerClosedHandler}
-        />
-        <main className={classes.Content}>
-            {this.props.children}
-        </main>
-    </Aux>
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
         )
     }
 }
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import NavigationItem from './NavigationItem/NavigationItem'
-import classes from './NavigationItems.css'
+import React from 'react';
 
-const navigationitems = (props) => (
-    <ul 
-     className={classes.NavigationItems}> 
-        <NavigationItem link="/" active>Buger Builder</NavigationItem>
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
         <NavigationItem link="/">Checkout</NavigationItem>
     </ul>
 );
 
-export default navigationitems;
+export default navigationItems;
